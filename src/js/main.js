3129a53619705c27520ac889a5163f3dc5393575
louispallett <louis.pallett@outlook.com>
2023-07-22 17:29:14 +0100

    Initial JS

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,8 @@ import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+const container = document.querySelector(".container");
+// console.log(container);
+
+container.textContent = "Odin: Todo List";
