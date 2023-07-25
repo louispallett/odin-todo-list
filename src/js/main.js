44854b2fb999dd13621d1f348f221cdc76b9291a
louispallett <louis.pallett@outlook.com>
2023-07-25 11:28:08 +0100

    Added to index.html accordion divs and basic styling for items.

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,3 @@ import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
-
-const container = document.querySelector(".container");
-// console.log(container);
-
-container.textContent = "Odin: Todo List";
