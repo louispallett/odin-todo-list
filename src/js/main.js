2d4bc1f8eee55a0052e6a09a5785e0469454a0af
louispallett <louis.pallett@outlook.com>
2023-07-26 13:35:15 +0100

    Removed popover and replaced with accordion - form for which started

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,11 @@
 // Import our custom CSS
 import '../scss/styles.scss'
 
-import { createPopper } from '@popperjs/core';
-
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap' 
 
-const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
-const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
+
+const newItem = (() => {
+    const container = document.getElementById("accordionExample");
+    console.log(container);
+});
