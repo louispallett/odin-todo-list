b429d7c0894575a41c11ac8e2abaf5d6588504c9
louispallett <louis.pallett@outlook.com>
2023-07-26 08:53:45 +0100

    Custom popover on #new and removed 'Completed/Date', moved to title. Some basic colouring

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,10 @@
 // Import our custom CSS
 import '../scss/styles.scss'
 
+import { createPopper } from '@popperjs/core';
+
 // Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
+import * as bootstrap from 'bootstrap' 
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
