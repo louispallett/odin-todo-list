ff50ecdb449c5192db2b70184c5ccbab4a2de1de
louispallett <louis.pallett@outlook.com>
2023-07-27 14:38:38 +0100

    Began class JS for items

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,8 @@ import '../scss/styles.scss'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap' 
 
+let items = [];
+
 // const newItem = (() => {
 //     const container = document.getElementById("accordionExample");
 //     console.log(container);
@@ -14,8 +16,40 @@ const submitNewItem = (() => {
 
     submitItem.addEventListener("click", (event) => {
         event.preventDefault();
+
+        // New item function
     });
+
+
 })();
+
+class Item {
+    constructor(title, notes, priority, deadline) {
+        this.title = title;
+        this.notes = notes;
+        this.priority = priority;
+        this.deadline = deadline;
+    }
+
+    // Not loading function!
+    addItem = () => {
+        // items.push(this);
+        console.log(this);
+    }
+
+    // pushItem = () => {
+    //     // See odin-library (classes)
+    //     for(let i = 0; i < )
+    // }
+}
+
+const myItem = new Item("Clean Kitchen", "Clean the kitchen, bro", "High", "22/07/2023");
+// items.push(myItem);
+
+console.log(myItem);
+console.log(items);
+
+
 
 // const highPrio = document.getElementById("btnradio2");
 // console.log(highPrio);
