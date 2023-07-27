b879d5520178c5b04ab93bd922eb7e6016d41605
louispallett <louis.pallett@outlook.com>
2023-07-27 13:58:46 +0100

    #newTitle maxlength

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,6 @@ import '../scss/styles.scss'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap' 
 
-
 // const newItem = (() => {
 //     const container = document.getElementById("accordionExample");
 //     console.log(container);
