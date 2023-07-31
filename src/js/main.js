9e752406849afcdefda2cd6da171cfd0ceea0174
louispallett <louis.pallett@outlook.com>
2023-07-31 20:18:29 +0100

    Each item (in Item class) now appends full information to the DOM via pushItem()

--- src/js/main.js
+++ src/js/main.js
@@ -33,7 +33,7 @@ class Item {
         this.deadline = deadline;
 
         this.addItem();
-        this.pushItem(title, deadline)
+        this.pushItem(title, description, deadline, priority)
     }
 
     addItem = () => {
@@ -41,21 +41,23 @@ class Item {
         console.log(items);
     }
 
-    pushItem = (title, deadline) => {  
-        const accordionContainer = document.getElementById("accordionExample");
+    pushItem = (title, description, deadline, itemPriority) => {      
+            const accordionContainer = document.getElementById("accordionExample");
 
-        // Accordion Header
+        // New Accordion Item (whole wrapper)
             const accordionItem = document.createElement("div");
             accordionItem.classList.add("accordion-item");
+
+        // Accordion Header
             const accordionHeader = document.createElement("h2");
             accordionHeader.classList.add("accordion-header");
             const accordionButton = document.createElement("button");
             accordionButton.classList.add("accordion-button", "collapsed");
             accordionButton.type = "button";
-            accordionButton["data-bs-toggle"] = "collapse";
-            accordionButton["data-bs-target"] = "#collapseOne";
-            accordionButton["aria-expanded"] = "true";
-            accordionButton["aria-controls"] = "collapseOne";
+            accordionButton.setAttribute("data-bs-toggle", "collapse");
+            accordionButton.setAttribute("data-bs-target", `#collapse${items.length}`);
+            accordionButton.setAttribute("aria-expanded", "true");
+            accordionButton.setAttribute("aria-controls", `collapse${items.length}`);            
             const buttonWrapper = document.createElement("div");
             buttonWrapper.classList.add("button-wrapper");
             const titleElement = document.createElement("div");
@@ -71,9 +73,86 @@ class Item {
             accordionItem.appendChild(accordionHeader);
         
         // Accordion Body
-
+            // Body Description
+            const collapseElement = document.createElement("div");
+            collapseElement.classList.add("accordion-collapse", "collapse");
+            collapseElement.setAttribute("id", `collapse${items.length}`);
+            const bodyTitleWrapper = document.createElement("div");
+            bodyTitleWrapper.classList.add("title-wrapper");
+            const descriptionElement = document.createElement("div");
+            descriptionElement.classList.add("accordion-body");
+            descriptionElement.setAttribute("id", "description");
+            descriptionElement.textContent = `${description}`;
+            bodyTitleWrapper.appendChild(descriptionElement);
+            collapseElement.appendChild(bodyTitleWrapper);
             
+            // Body Priority
+            const priorityElement = document.createElement("div");
+            priorityElement.setAttribute("id", "priorities");
+            const priorityTitle = document.createElement("div");
+            priorityTitle.textContent = "Priority:"
+            priorityElement.appendChild(priorityTitle);
+            const priorityBtnGrp = document.createElement("div");
+            priorityBtnGrp.classList.add("btn-group");
+            priorityBtnGrp.setAttribute("role", "group");
+            priorityBtnGrp.setAttribute("aria-label", "Basic radio toggle button group");
+            priorityBtnGrp.setAttribute("id", "priorities-group");
 
+            const radioInput1 = document.createElement("input");
+
+            radioInput1.setAttribute("type", "radio");
+            radioInput1.setAttribute("class", "btn-check");
+            radioInput1.setAttribute("name", "btnradio");
+            radioInput1.setAttribute("id", "btnradio3");
+            radioInput1.setAttribute("autocomplete", "off");
+            const label1 = document.createElement("label");
+            label1.setAttribute("class", "btn btn-outline-success");
+            label1.setAttribute("for", "btnradio3");
+            label1.textContent = "Low";
+
+            // Create the second radio input and its corresponding label
+            const radioInput2 = document.createElement("input");
+            radioInput2.setAttribute("type", "radio");
+            radioInput2.setAttribute("class", "btn-check");
+            radioInput2.setAttribute("name", "btnradio");
+            radioInput2.setAttribute("id", "btnradio2");
+            radioInput2.setAttribute("autocomplete", "off");
+            const label2 = document.createElement("label");
+            label2.setAttribute("class", "btn btn-outline-warning");
+            label2.setAttribute("for", "btnradio2");
+            label2.textContent = "Medium";
+
+            // Create the third radio input and its corresponding label
+            const radioInput3 = document.createElement("input");
+            radioInput3.setAttribute("type", "radio");
+            radioInput3.setAttribute("class", "btn-check");
+            radioInput3.setAttribute("name", "btnradio");
+            radioInput3.setAttribute("id", "btnradio1");
+            radioInput3.setAttribute("autocomplete", "off");
+            const label3 = document.createElement("label");
+            label3.setAttribute("class", "btn btn-outline-danger");
+            label3.setAttribute("for", "btnradio1");
+            label3.textContent = "High";
+
+            if(itemPriority == "low") {
+                radioInput1.checked = true;
+            } else if (itemPriority == "medium") {
+                radioInput2.checked = true;
+            } else {
+                radioInput3.checked = true;
+            }
+
+            priorityBtnGrp.appendChild(radioInput1);
+            priorityBtnGrp.appendChild(label1);
+            priorityBtnGrp.appendChild(radioInput2);
+            priorityBtnGrp.appendChild(label2);
+            priorityBtnGrp.appendChild(radioInput3);
+            priorityBtnGrp.appendChild(label3);
+
+            priorityElement.appendChild(priorityBtnGrp);
+            collapseElement.appendChild(priorityElement);
+            
+            accordionItem.appendChild(collapseElement);
         accordionContainer.appendChild(accordionItem);
 
     }
@@ -89,9 +168,8 @@ class Item {
     // }
 }
 
-const myItem = new Item("Clean Kitchen", "Clean the kitchen, bro", "High", "22/07/2023");
-// console.log(myItem);
-const myItem2 = new Item("Bills", "Pay the water, gas, and electricity bill", "High", "11/05/2023");
+// const myItem = new Item("Clean Kitchen", "Clean the kitchen, bro", "low", "22/07/2023");
+// const myItem2 = new Item("Bills", "Pay the water, gas, and electricity bill", "Low", "11/05/2023");
 
 
 
