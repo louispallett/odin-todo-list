28343c888f4e0767799cf66d9a560693fb2a6be0
louispallett <louis.pallett@outlook.com>
2023-08-23 12:09:45 +0100

    Changed localStorage theme name to 1zUserTheme so it doesn't clash with title names

--- src/js/themes.js
+++ src/js/themes.js
@@ -4,10 +4,10 @@ const html = document.querySelector("html");
 
 const themeDetector = (() => {
     // If user has already selected a theme, get it and apply it to conditional statement below
-    const userTheme = localStorage.getItem("theme");
-    if(userTheme === "user theme dark") {
+    const userTheme = localStorage.getItem("1zUserTheme");
+    if(userTheme === "dark") {
         html.setAttribute("data-bs-theme", "dark");
-    } else if (userTheme === "user theme light") {
+    } else if (userTheme === "light") {
         html.setAttribute("data-bs-theme", "light");
     } else { //Or, in other words, set to "null" (as in no theme originally chosen)
         if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
@@ -20,10 +20,10 @@ const themeDetector = (() => {
 
 const setDarkTheme = (() => {
     html.setAttribute("data-bs-theme", "dark");
-    localStorage.setItem("theme", "user theme dark");
+    localStorage.setItem("1zUserTheme", "dark");
 });
 
 const setLightTheme = (() => {
     html.setAttribute("data-bs-theme", "light");
-    localStorage.setItem("theme", "user theme light");
+    localStorage.setItem("1zUserTheme", "light");
 });
